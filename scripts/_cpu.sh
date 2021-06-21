e4d55287be751257def5e3868b7789f3bf935789
CatDevz <codyquinn1122@gmail.com>
2021-06-21 13:19:04 -0700

    :bug: Fixed bug in 1 script

--- scripts/_cpu.sh
+++ scripts/_cpu.sh
@@ -11,8 +11,8 @@ DI=" "
 # Fetching current CPU state
 TEMP="$(sensors | grep -e 'id 0\:' | awk '{print $4}' | sed 's/[^0-9\.]//g;s/\.[0-9]//g')"
 
-[ $TEMP -lt 30 ] && STATE="${COOL}" && TSTATE="${CI}"
-[ $TEMP -gt 30 ] && [ $TEMP -lt 70 ] && STATE="${WARN}" && TSTATE="${WI}"
+[ $TEMP -le 40 ] && STATE="${COOL}" && TSTATE="${CI}"
+[ $TEMP -gt 40 ] && [ $TEMP -lt 70 ] && STATE="${WARN}" && TSTATE="${WI}"
 [ $TEMP -ge 70 ] && STATE="${DANGER}" && TSTATE="${DI}"
 
 echo "${STATE}${TEMP}°C ${TSTATE}^d^  "
